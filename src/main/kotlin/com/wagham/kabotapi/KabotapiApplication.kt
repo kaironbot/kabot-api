24514821263175992ed0a9eca87be37f3019d365
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-09-24 20:57:37 +0200

    Init with start spring

---
+++ src/main/kotlin/com/wagham/kabotapi/KabotapiApplication.kt
@@ -0,0 +1,11 @@
+package com.wagham.kabotapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KabotapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KabotapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/wagham/kabotapi/KabotapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wagham.kabotapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KabotapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
