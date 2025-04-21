8621da4d9b6c5c9a474e87d32468424011999c7a
Vincenzo Pierro <vcp@icure.com>
2025-04-21 11:36:07 +0200

    Revert "Use TCP socket instead of UDP for commands"
    This reverts commit d61242bc5c259b626047ec96bb49d60063addee4.

--- src/main/kotlin/org/wagham/kabotapi/components/InstanceInactivityManager.kt
+++ src/main/kotlin/org/wagham/kabotapi/components/InstanceInactivityManager.kt
@@ -51,7 +51,7 @@ class InstanceInactivityManager(
 
 	private fun listenForZombies() = managerScope.launch {
 //		delay(instanceTtl)
-		doInfinity("0 25 * * * *") {
+		doInfinity("0 0 * * * *") {
 			try {
 				commandComponent.sendSocketCommand(Pm2ListCommand()).filter {
 					it.isActive && it.name !in excludedInstances
