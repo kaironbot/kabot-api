eddf7692dbb9b8e03420423703a7626cffb70592
Vincenzo Pierro <vcp@icure.com>
2025-04-24 21:41:44 +0200

    increased wait before redirect

--- src/main/kotlin/org/wagham/kabotapi/controllers/FoundryController.kt
+++ src/main/kotlin/org/wagham/kabotapi/controllers/FoundryController.kt
@@ -29,7 +29,7 @@ fun Routing.foundryController() = route("/foundry") {
 			val instanceUrl = checkNotNull(call.parameters["instanceUrl"]) {
 				"Instance URL must not be null"
 			}
-			delay(1.seconds)
+			delay(3.seconds)
 			call.respondRedirect(foundryLogic.startInstance(instanceUrl), permanent = false)
 		}
 	}
@@ -39,7 +39,7 @@ fun Routing.foundryController() = route("/foundry") {
 			val instanceUrl = checkNotNull(call.parameters["instanceUrl"]) {
 				"Instance URL must not be null"
 			}
-			delay(1.seconds)
+			delay(3.seconds)
 			call.respondRedirect(foundryLogic.startInstance(instanceUrl), permanent = false)
 		}
 	}
